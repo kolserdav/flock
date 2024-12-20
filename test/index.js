9d7c29a1da6412e8d1239b9a90053f1465cd7b6c
kolserdav <serega12101983@gmail.com>
2024-12-20 16:48:02 +0700

    fixed tet

--- test/index.js
+++ test/index.js
@@ -4,6 +4,6 @@ const { Flock } = require("../index.cjs");
 const filePath = path.resolve("tmp/das");
 const flock = new Flock(filePath);
 console.log("Lock file 1", filePath);
-flock.flock();
+flock.lock();
 console.log("Lock file 2", filePath);
 while (true) {}
