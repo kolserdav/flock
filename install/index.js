853aa693a05a5bc76f8d2438330d41bc33bdf79f
kolserdav <serega12101983@gmail.com>
2025-04-27 14:09:09 +0700

    fixed check musl

--- install/index.js
+++ install/index.js
@@ -1,15 +1,20 @@
 const os = require('os');
+const { family, MUSL } = require('detect-libc');
 
 const versions = {
   'aarch64-unknown-linux-musl': 'ARM64 Linux with musl 1.2.3',
   'x86_64-unknown-linux-musl': '64-bit Linux with musl 1.2.3',
 };
 
-function getArchitecture() {
+async function getArchitecture() {
   const arch = os.arch();
   const platform = os.platform();
 
   if (platform === 'linux') {
+    if ((await family()) !== MUSL) {
+      return null;
+    }
+
     if (arch === 'arm64') {
       return 'aarch64-unknown-linux-musl';
     } else if (arch === 'amd64') {
@@ -19,10 +24,12 @@ function getArchitecture() {
   return null;
 }
 
-const architecture = getArchitecture();
+(async () => {
+  const architecture = await getArchitecture();
 
-if (architecture && versions[architecture]) {
-  console.log(`Download version: ${architecture} - ${versions[architecture]}`);
-} else {
-  console.log('Unknown arch or os');
-}
+  if (architecture && versions[architecture]) {
+    console.log(`Download version: ${architecture} - ${versions[architecture]}`);
+  } else {
+    console.log('Unknown arch or os');
+  }
+})();
